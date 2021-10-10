e125aa8641d4766af049049ffdf08ed83a1e6fc4
ipisano <daniele.logli@gmail.com>
2021-10-10 23:25:48 +0200

    Add files via upload
    start fixing norm puzzle_solver.c

--- puzzle_solver.c
+++ puzzle_solver.c
@@ -6,29 +6,31 @@
 /*   By: gdel-giu <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/09 20:38:08 by gdel-giu          #+#    #+#             */
-/*   Updated: 2021/10/10 14:44:29 by gdel-giu         ###   ########.fr       */
+/*   Updated: 2021/10/10 23:25:25 by dlogli           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
+#include <unistd.h>
 
-enum e_dir
+enum	e_dir
 {
 	UP,
-    DOWN,
+	DOWN,
 	LEFT,
-    RIGHT
+	RIGHT
 };
 
-void frame_builder(int *buffer, int x, int y, int dir, int mtx[6][6]);
+void	frame_builder(int *buffer, int x, int y, int dir, int mtx[6][6]);
 
-void check_obvious(int mtx[6][6], int x, int y, int start, int dir);
+void	check_obvious(int mtx[6][6], int x, int y, int start, int dir);
 
-void empty_tab(int mtx[6][6])
+void	empty_tab(int mtx[6][6])
 {
-	int i = 1;
-	int j = 1;
+	int	i;
+	int	j;
 
+	i = 1;
+	j = 1;
 	while (i <= 4)
 	{
 		while (j <= 4)
@@ -41,39 +43,40 @@ void empty_tab(int mtx[6][6])
 	}
 }
 
-int absolute(int val)
+int	absolute(int val)
 {
 	if (val < 0)
 		val = -val;
 	return (val);
 }
 
-void puzzle_solver(int *values, int mtx[6][6])
+void	puzzle_solver(int *values, int mtx[6][6])
 {
-	int i;
-	int max_iters = 2;
+	int	i;
+	int	max_iters;
 
-	empty_tab(mtx);
-    frame_builder(values, 1, 0, RIGHT, mtx);
-    frame_builder(values + 4, 1, 5, RIGHT, mtx);
-    frame_builder(values + 8, 0, 1, DOWN, mtx);
-    frame_builder(values + 12, 5, 1, DOWN, mtx);
+	max_iters = 16
+		empty_tab(mtx);
+	frame_builder(values, 1, 0, RIGHT, mtx);
+	frame_builder(values + 4, 1, 5, RIGHT, mtx);
+	frame_builder(values + 8, 0, 1, DOWN, mtx);
+	frame_builder(values + 12, 5, 1, DOWN, mtx);
 	i = 0;
 	while (i < max_iters)
 	{
-	check_obvious(mtx, 0, 1, 1, UP);
-	check_obvious(mtx, 5, 0, 4, DOWN);
-	check_obvious(mtx, 1, 0, 1, LEFT);
-    check_obvious(mtx, 0, 5, 4, RIGHT);
-	i++;
+		check_obvious(mtx, 0, 1, 1, UP);
+		check_obvious(mtx, 5, 0, 4, DOWN);
+		check_obvious(mtx, 1, 0, 1, LEFT);
+		check_obvious(mtx, 0, 5, 4, RIGHT);
+		i++;
 	}
 }
 
-void check_obvious(int mtx[6][6], int x, int y, int start, int dir)
+void	check_obvious(int mtx[6][6], int x, int y, int start, int dir)
 {
-	int scan;
-	int delta;
-	int iter;
+	int	scan;
+	int	delta;
+	int	iter;
 
 	scan = start;
 	delta = 0;
@@ -101,7 +104,7 @@ void check_obvious(int mtx[6][6], int x, int y, int start, int dir)
 				if (dir == UP)
 					mtx[x + 1][y] = 4;
 				else
-					mtx[x - 1][y] = 4; 
+					mtx[x - 1][y] = 4;
 			}
 			if (mtx[x][y] == 3)
 			{
@@ -173,9 +176,11 @@ void check_obvious(int mtx[6][6], int x, int y, int start, int dir)
 						while (scan <= 4)
 						{
 							//printf("%d %d = %d\n", scan, y, mtx[scan][y]);
-							if (mtx[y][5 - scan] == 1){
+							if (mtx[y][5 - scan] == 1)
+							{
 								mtx[y + 1][x] = 2;
-								printf("%d %d = %d\n", scan, y, mtx[scan][y]);}
+								printf("%d %d = %d\n", scan, y, mtx[scan][y]);
+							}
 							else if (mtx[y][y] == 2)
 								mtx[y - 1][x] = 1;
 							scan++;
@@ -192,8 +197,10 @@ void check_obvious(int mtx[6][6], int x, int y, int start, int dir)
 							if (mtx[x][5 - scan] == 1)
 								mtx[x + 1][y] = 2;
 							else if (mtx[5 - scan][y] == 2)
+							{
 								mtx[x + 1][y] = 1;
-							scan++;
+								scan++;
+							}
 						}
 					}
 				}
@@ -203,20 +210,19 @@ void check_obvious(int mtx[6][6], int x, int y, int start, int dir)
 	}
 }
 
-void    frame_builder(int *buffer, int x, int y, int dir, int mtx[6][6])
+void	frame_builder(int *buffer, int x, int y, int dir, int mtx[6][6])
 {
-    int a;
-    
-    a = 0;
-    while (a < 4)
-    {
-        mtx[y][x] = *buffer;
-        buffer++;
-        if (dir == DOWN)
-            y++;
-        if (dir == RIGHT)
-            x++;
-        a++;
-    }
-}
+	int	a;
 
+	a = 0;
+	while (a < 4)
+	{
+		mtx[y][x] = *buffer;
+		buffer++;
+		if (dir == DOWN)
+			y++;
+		if (dir == RIGHT)
+			x++;
+		a++;
+	}
+}
